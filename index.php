328db7170c30bf4cea8850a7015313a8aab242d6
Shuna322 <Sashka-pokudko@outlook.com>
2018-06-09 14:33:39 +0300

    added working carousel with database

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<?php
+    require_once("db_connect.php");
+    $query = "select * from carousel";
+    $response = mysqli_query($dbc, $query);
+    $counter = 1;
+  ?>
 <!DOCTYPE html>
 <html lang="ua">
 
@@ -107,10 +113,42 @@
   <content>
     <div class="container">
       <div class="row">
-        <div class="col-lg-8 offset-lg-2">
-          <div id="carouselExampleIndicators" class="carousel slide carousel-fade" data-ride="carousel">
-            <div class="carousel-inner">
-              <div class="carousel-item active">
+        <div class="col-sm-10 offset-sm-1">
+          <div id="carousel" class="carousel slide carousel-fade" data-ride="carousel">
+              <?php
+                  if ($response){
+                    while ($row = mysqli_fetch_assoc($response)) {
+                      $image=$row ['picture'];
+                      if ($counter == 1){
+                        // carousel counter
+                        echo '<ol class="carousel-indicators">
+                              <li data-target="#carousel" data-slide-to="0" class="active"></li>';
+                            for ($i=1; $i < mysqli_num_rows($response); $i++){
+                              echo '<li data-target="#carousel" data-slide-to="'.$i.'"></li>';
+                            }
+                              echo' </ol>
+                               <div class="carousel-inner">';
+                               ////////////////////////////
+                      echo '<div class="carousel-item active">
+                        <img class="d-block w-100" src="data:image/jpeg;base64,'.base64_encode($row["picture"]).'" alt="Slide #'.$counter.'">
+                      </div>';
+                      $counter++;
+                    } else {
+                      echo '<div class="carousel-item">
+                      <img class="d-block w-100" src="data:image/jpeg;base64,'.base64_encode($row["picture"]).'" alt="Slide #'.$counter.'">
+                      </div>';
+                      $counter++;
+                      }
+                    }
+                  } else {
+                    echo 'Помилка';
+                    echo mysqli_error($dbc);
+                  }
+
+                  mysqli_close ($dbc);
+
+               ?>
+              <!-- <div class="carousel-item active">
                 <img class="d-block w-150" src="https://i.ytimg.com/vi/sNJOisDTYEg/maxresdefault.jpg" alt="First slide">
               </div>
               <div class="carousel-item">
@@ -118,13 +156,13 @@
               </div>
               <div class="carousel-item">
                 <img class="d-block w-150" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSt40yn47VONeyoPmG2978v8r-wE-ElJw9oPrCGW8fA1njUZHm" alt="Third slide">
-              </div>
+              </div> -->
             </div>
-            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <a class="carousel-control-prev" href="#carousel" role="button" data-slide="prev">
               <span class="carousel-control-prev-icon" aria-hidden="true"></span>
               <span class="sr-only">Previous</span>
             </a>
-            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <a class="carousel-control-next" href="#carousel" role="button" data-slide="next">
               <span class="carousel-control-next-icon" aria-hidden="true"></span>
               <span class="sr-only">Next</span>
             </a>
